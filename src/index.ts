d6ee878794c99479f5469d3ff05c517b523f373b
Ryan Loader <ryan.loader@cazoo.co.uk>
2021-03-19 09:24:59 +0000

    Fix import ordering

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Is } from './is'
 import * as Naming from './naming'
 import { param, store } from './reader'
-import { getConfig, getConfig2 } from './ssm'
 import { cfg } from './reader2'
+import { getConfig, getConfig2 } from './ssm'
 
 export { Is, Naming, getConfig, param, store, getConfig2, cfg }
